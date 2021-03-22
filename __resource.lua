6fb9641203ca2c520ca6effd4b2e14be4777520f
payn0r <79810631+payn0r@users.noreply.github.com>
2021-03-22 11:19:22 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+desctription 'Furman Corner ID'
+
+client_script 'client/main.lua'
+
+ui_page ('client/html/UI.html')
+
+files({
+    'client/html/UI.html',
+    'client/html/style.css',
+    'client/html/kenyan_coffee_rg.ttf',
+    'client/html/logo.png'
+})

c23651da5c448a1ba84a47068851c6616d43b350
payn0r <79810631+payn0r@users.noreply.github.com>
2021-03-25 10:43:57 +0100

    fixed pokazywanie id v2

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 desctription 'Furman Corner ID'
 
-client_script 'client/main.lua'
+client_script 'client/client.lua'
 
 ui_page ('client/html/UI.html')
 
